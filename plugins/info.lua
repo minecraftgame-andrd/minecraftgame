11da02e3e8436d4452662e469b36dbd4490e9b9d
hacker44 <hacker.ht44@gmail.com>
2016-03-19 14:43:17 +0330

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -57,7 +57,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'https://telegram.me/TaylorTg_ch'
+  text = text..'@BHH_team'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, extra.user..' نام کاربری مورد نظر یافت نشد.', ok_cb, false)
@@ -96,7 +96,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'https://telegram.me/TaylorTg_ch'
+  text = text..'@BHH_team'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
@@ -135,7 +135,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'https://telegram.me/TaylorTg_ch'
+  text = text..'@BHH_team'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -208,7 +208,7 @@ local function run(msg, matches)
 	 text = text..'نام گروه : '..msg.to.title..'\n'
      text = text..'ایدی گروه : '..msg.to.id
     end
-	text = text..'\n\n https://telegram.me/TaylorTg_ch'
+	text = text..'\n\n @BHH_team'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
