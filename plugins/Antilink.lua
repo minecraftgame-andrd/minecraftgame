f0e2347d9db908701ec7319fcfbf9b67a084691a
hacker44 <hacker.ht44@gmail.com>
2016-03-26 12:54:59 +0430

    Update Antilink.lua

--- plugins/Antilink.lua
+++ plugins/Antilink.lua
@@ -1,12 +1,30 @@
-local function run(msg)
-if not is_momod(msg) and msg.type = 'chat' then
---chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-return 'No links here!'
-     end 
-end 
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_ads'] then
+                lock_ads = data[tostring(msg.to.id)]['settings']['lock_ads']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_ads == "yes" then
+        send_large_msg(chat, 'Ø§Ø±Ø³Ø§Ù„ Ù„ÛŒÙ†Ú© ØºÛŒØ± Ù…Ø¬Ø§Ø² Ù‡Ø³Øª ðŸš¨ðŸš¨ðŸš¨')
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ 
 return {patterns = {
-".com",
-"[Hh][Tt][Tt][Pp][:][/][/]",
-"[Hh][Tt][Tt][Pp][Ss][:][/][/]",
-"adf.ly"
-}, run = run}
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp]://",
+"[Ww][Ww][Ww]:",
+  },
+  run = run
+}
