da28b10e8ccab5b1123b10646fc05971b1bd2c3c
hacker44 <hacker.ht44@gmail.com>
2016-04-01 13:20:21 +0430

    Update clash.lua

--- plugins/clash.lua
+++ plugins/clash.lua
@@ -1,16 +1,7 @@
---[[
-
-#
-#       @GPMod
-#   @dragon_born
-#
-
-]]
-
 local apikey = 
-'Enter Your Clash API here' 
+'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYwNGZhODU1LWQyZTgtNDQ3ZS05ODM3LTBjZmJjYzcyMjE3NSIsImlhdCI6MTQ1OTI2NjYwMSwic3ViIjoiZGV2ZWxvcGVyL2JiNzM4NDQxLWU3YzUtZThmMi0wZDljLWQyMGE4YWExNGI1NCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjUuMjAwLjIwOS4yNyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.Y2r6kRVtThZ5hgPl60YQnD5CM_jUmVnieRLJEixnkaV4KqzOdX_RKJGnEkQfMjMXzmtRaQ30HzQf3WCn9BowVA' 
 local function run(msg, matches)
- if matches[1]:lower() == 'clan' or matches[1]:lower() == 'clash' or matches[1]:lower() == 'clantag' or matches[1]:lower() == 'tag' then
+ if matches[1]:lower() == 'clash' then
   local clantag = matches[2]
   if string.match(matches[2], '^#.+$') then
      clantag = string.gsub(matches[2], '#', '')
@@ -25,21 +16,21 @@ if jdat.reason then
       if jdat.reason == 'accessDenied' then return 'برای ثبت API Key خود به سایت زیر بروید\ndeveloper.clashofclans.com' end
    return '#Error\n'..jdat.reason
   end
-  local text = 'Clan Tag: '.. jdat.tag
-     text = text..'\nClan Name: '.. jdat.name
-     text = text..'\nDescription: '.. jdat.description
-     text = text..'\nType: '.. jdat.type
-     text = text..'\nWar Frequency: '.. jdat.warFrequency
-     text = text..'\nClan Level: '.. jdat.clanLevel
-     text = text..'\nWar Wins: '.. jdat.warWins
-     text = text..'\nClan Points: '.. jdat.clanPoints
-     text = text..'\nRequired Trophies: '.. jdat.requiredTrophies
-     text = text..'\nMembers: '.. jdat.members
-     text = text..'\n\n@BHH_team'
+  local text = 'تگ کلن: '.. jdat.tag
+     text = text..'\nنام کلن: '.. jdat.name
+     text = text..'\nوضعیت عضو گیری: '.. jdat.type
+     text = text..'\nوضعیت وار: '.. jdat.warFrequency
+     text = text..'\nموقعیت مکانی: '..jdat.location.name
+     text = text..'\nلول : '.. jdat.clanLevel
+     text = text..'\nوار های پیروز: '.. jdat.warWins
+     text = text..'\nامتیاز کلن: '.. jdat.clanPoints
+     text = text..'\nحداقل امتیاز مورد نیاز: '.. jdat.requiredTrophies
+     text = text..'\nافراد: '.. jdat.members..'نفر' 
+	 text = text..'\nدرباره کلن: '.. jdat.description
      cmd:close()
   return text
  end
- if matches[1]:lower() == 'members' or matches[1]:lower() == 'clashmembers' or matches[1]:lower() == 'clanmembers' then
+ if matches[1]:lower() == 'clash>' then
   local members = matches[2]
   if string.match(matches[2], '^#.+$') then
      members = string.gsub(matches[2], '#', '')
@@ -56,34 +47,27 @@ if jdat.reason then
   local leader = ""
   local coleader = ""
   local items = jdat.items
-  leader = 'Clan Moderators: \n'
+  leader = 'مدیران کلن: \n'
    for i = 1, #items do
    if items[i].role == "leader" then
-   leader = leader.."\nLeader: "..items[i].name.."\nLevel: "..items[i].expLevel
+   leader = leader.."\nلیدر: "..items[i].name.."\nلول: "..items[i].expLevel.."..items[i].league.name
    end
    if items[i].role == "coLeader" then
-   coleader = coleader.."\nCo-Leader: "..items[i].name.."\nLevel: "..items[i].expLevel
+   coleader = coleader.."\nکو لیدر: "..items[i].name.."\nلول: "..items[i].expLevel
    end
   end
-text = leader.."\n"..coleader.."\n\nClan Members:"
+text = leader.."\n"..coleader.."\n\nاعضا کلن:"
   for i = 1, #items do
   text = text..'\n'..i..'- '..items[i].name..'\nlevel: '..items[i].expLevel.."\n"
   end
-  text = text.."\n\n@BHH_team"
    cmd:close()
   return text
  end
 end
-
 return {
    patterns = {
-"^[/!](clash) (.*)$",
-"^[/!](clan) (.*)$",
-"^[/!](clantag) (.*)$",
-"^[/!](tag) (.*)$",
-"^[/!](clashmembers) (.*)$",
-"^[/!](clanmembers) (.*)$",
-"^[/!](members) (.*)$",
+"^([Cc]lash) (.*)$",
+"^([Cc]lash>) (.*)$",
    },
    run = run
 }
